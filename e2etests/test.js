7414cb0909ef495ad7404e5848ba03317a65d60d
mon7792 <monish7792@gmail.com>
2019-03-05 22:01:23 +0000

    Formatting the code to follow airBnb style guide

--- e2etests/test.js
+++ e2etests/test.js
@@ -1,10 +1,10 @@
-const expect = require('chai').expect;
+/* global describe, it, browser */
+const { expect } = require('chai');
 
 describe('TodoList App', () => {
   it('Should load with the right title', () => {
     browser.url('http://localhost:3000/');
     const actualTitle = browser.getTitle();
-    console.log(actualTitle);
     expect(actualTitle).to.eql('localhost');
   });
 });
